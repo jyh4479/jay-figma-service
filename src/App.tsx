f9fa5dd32eb8a1b144cf6ec705d24c5cdead2c4c
jyh4479 <jyh.dev@gmail.com>
2024-03-10 00:28:03 +0900

    [ADD] router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css'
-import {figmaTokenCallback} from "./apis/token";
-import getFigmaToken from "./apis/token/getFigmaToken.ts";
+
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
 
     return (
-        <div>
-            <div>Figma Test</div>
-            <button onClick={() => figmaTokenCallback()}>Call back</button>
-            <button onClick={() => getFigmaToken()}>Get Token</button>
-        </div>
+        <Routes>
+            <Route path={"/"} element={<div>home</div>}/>
+            <Route path={"/test"} element={<div>test</div>}/>
+        </Routes>
+        // <div>
+        //     <div>Figma Test</div>
+        //     <button onClick={() => figmaTokenCallback()}>Call back</button>
+        //     <button onClick={() => getFigmaToken()}>Get Token</button>
+        // </div>
     )
 }
 
--- src/apis/token/getFigmaToken.ts
+++ src/apis/token/getFigmaToken.ts
@@ -5,8 +5,11 @@ const secret = "WHahrL8pQrnQIcl1e8xLueIIlcVv78";
 const redirectUri = "http://localhost:5173/figma-token"
 const grantType = "authorization_code";
 
+//FIXME: secret 정보 env 파일로 빼놓기 or 서비스 사용자에게 입력 받을 수 있는 방법 생각하기
+//상단 key는 현재 없앤 상태임
+
 const getFigmaToken = () => {
-    return postAxios(`https://www.figma.com/api/oauth/token?client_id=${clientId}&client_secret=${secret}&redirect_uri=${redirectUri}&code=s4r53jeXD4RXBI3NaxkUNDO5l&grant_type=${grantType}`);
+    return postAxios(`https://www.figma.com/api/oauth/token?client_id=${clientId}&client_secret=${secret}&redirect_uri=${redirectUri}&code=3tK9nNye2FaFl7ptYHbqiu9f5&grant_type=${grantType}`);
 
 }
 
