ceff8ec12122de5791301d417a18a5a864c2f0c1
jyh4479 <jyh.dev@gmail.com>
2024-03-12 22:31:32 +0900

    [test] get parameter

--- src/pages/FigmaCodePage/index.tsx
+++ src/pages/FigmaCodePage/index.tsx
@@ -1,4 +1,12 @@
+import {useSearchParams} from "react-router-dom";
+
 const FigmaCodePage = () => {
+
+    const [params] = useSearchParams();
+
+    console.log(params.get('code'));
+    console.log(params.get('state'));
+
     return (
         <div>figma code</div>
     )
